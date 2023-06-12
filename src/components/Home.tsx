102e6c72b92b0c6f6944d8ceecf30b1a299da542
Ghulam Rabbani <rabbani4007@gmail.com>
2023-06-12 15:18:16 +0530

    updated the code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="jumbotron">
       <h1 className="display-4">Hello, My React  app!</h1>
       <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
+        This is a simple app, a simple jumbotron-style component for
         calling extra attention to featured content or information.
       </p>
       <hr className="my-4" />
